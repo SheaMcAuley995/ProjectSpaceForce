48e3f85485e9fa8c7c5b7e8ca05ae89ced20f3b0
Shea McAuley <s179049@students.aie.edu.au>
2019-01-21 23:13:58 -0800

    Almost forgot to push this

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,8 +8,6 @@ public class PlayerController : MonoBehaviour {
     public float runSpeed = 6;
     public float gravity = -12;
     public float jumpheight = 1;
-    [Range(0,1)]
-    public float airControlPercent;
 
     public float turnSmoothTime = 0.2f;
     float turnSmoothVelocity;
@@ -21,11 +19,11 @@ public class PlayerController : MonoBehaviour {
 
     [SerializeField] Transform cameraTrans;
 
-    CharacterController controller;
+    Rigidbody rb;
 
     private void Start()
     {
-        controller = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update () {
@@ -35,11 +33,6 @@ public class PlayerController : MonoBehaviour {
 
         Move(inputDir, running);
 
-        if(Input.GetKey(KeyCode.Space))
-        {
-            Jump();
-        }
-
 	}
 
     void Move(Vector2 inputDir, bool running)
@@ -48,46 +41,32 @@ public class PlayerController : MonoBehaviour {
         if (inputDir != Vector2.zero)
         {
             float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg + cameraTrans.eulerAngles.y;
-            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, GetModifiedSmoothTime(turnSmoothTime));
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
         }
         
         float targetSpeed = ((running) ? runSpeed : walkSpeed) * inputDir.magnitude;
-        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, GetModifiedSmoothTime(speedSmoothTime));
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
 
-        velocityY += Time.deltaTime * gravity;
+        //velocityY += Time.deltaTime * gravity;
 
-        Vector3 velocity = transform.forward * currentSpeed + Vector3.up * velocityY;
+        rb.velocity = transform.forward * currentSpeed;
 
 
-        controller.Move(velocity * Time.deltaTime);
+        //controller.Move(rb.velocity * Time.deltaTime);
 
-        if (controller.isGrounded)
-        {
-            velocityY = 0;
-        }
+       //if (controller.isGrounded)
+       //{
+       //    velocityY = 0;
+       //}
     }
 
 
-    void Jump()
-    {
-        if(controller.isGrounded)
-        {
-            float jumpVelocity = Mathf.Sqrt(-2 * gravity * jumpheight);
-            velocityY = jumpheight;
-        }
-    }
-
-    float GetModifiedSmoothTime(float smoothTime)
-    {
-        if(controller.isGrounded)
-        {
-            return smoothTime;
-        }
-        if(airControlPercent == 0)
-        {
-            return float.MaxValue;
-        }
-
-        return smoothTime / airControlPercent;
-    }
+   // void Jump()
+   // {
+   //     if(controller.isGrounded)
+   //     {
+   //         float jumpVelocity = Mathf.Sqrt(-2 * gravity * jumpheight);
+   //         velocityY = jumpheight;
+   //     }
+   // }
 }
