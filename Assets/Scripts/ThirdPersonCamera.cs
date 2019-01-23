f803909f9d7331b76dcfd703c1e848547a0cab06
Shea McAuley <s179049@students.aie.edu.au>
2019-01-22 17:58:58 -0800

    Player UI

--- Assets/Scripts/ThirdPersonCamera.cs
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -7,14 +7,14 @@ public class ThirdPersonCamera : MonoBehaviour {
     public float mouseSensitivity = 10;
     public Transform target;
     public float dstFromTarget = 2;
-    public Vector2 pitchMinMax = new Vector2(-40, 85);
+    //public Vector2 pitchMinMax = new Vector2(-40, 85);
 
     public float rotationSmoothtime = .12f;
     Vector3 rotationSmoothVelocity;
     Vector3 currentRotation;
 
-    float yaw;
-    float pitch;
+    [SerializeField] float yaw;
+    [SerializeField] float pitch;
 
     private void Start()
     {
@@ -24,9 +24,9 @@ public class ThirdPersonCamera : MonoBehaviour {
 
     // Update is called once per frame
     void LateUpdate () {
-        yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
-        pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
-        pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+       //yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
+       //pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+       //pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
 
         currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref rotationSmoothVelocity, rotationSmoothtime);
         transform.eulerAngles = currentRotation;
