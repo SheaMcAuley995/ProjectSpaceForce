1ae99333918b066c41103e2e5f4919c97d6f27de
Michael Kaplor <mkaplor@gmail.com>
2019-01-22 17:47:44 -0800

    end of day, added pipes

--- Assets/MichaelTrash/Pipe.cs
+++ Assets/MichaelTrash/Pipe.cs
@@ -1,16 +1,61 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Interacted : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public enum PipeDamageLevels
+{
+    Fixed, MediumDamage, Broken
 }
+
+public class Pipe : Interactable
+{
+    PipeDamageLevels pipeDamage;
+    
+    private void Update()
+    {
+        CheckPipeStatus();
+        
+    }
+
+    private void CheckPipeStatus()
+    {
+        
+    }
+    void PipeStatus()
+    {
+        switch (pipeDamage)
+        {
+            case PipeDamageLevels.Fixed:
+                break;
+            case PipeDamageLevels.MediumDamage:
+                break;
+            case PipeDamageLevels.Broken:
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    #region Interact Functions
+    //wet electric goo blunt fire
+    public override void Wet()
+    { }
+    public override void Electric()
+    { }
+    public override void Goo()
+    { }
+    public override void Blunt()
+    { }
+    public override void Fire()
+    { }
+    #endregion
+
+}
+
+
+
+// goo makes break rate slower
+//broken pipe gradually reduces hull integrity
+//pipes leak water
